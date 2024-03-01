dff473618ce30bdd71b04d189562b4a2956b35d6
mtyrpula <113094723+mtyrpula@users.noreply.github.com>
2024-03-01 11:37:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic PHP Page</title>
+</head>
+<body>
+
+<?php
+// PHP code starts here
+echo "<h1>Hello, world!</h1>";
+echo "<p>This is a basic PHP page.</p>";
+?>
+
+</body>
+</html>
